af3e520eb9d2ce127d7af517b4e676e053aef2fc
tir_nguyen <t20051229@gmail.com>
2026-01-16 19:43:24 +0700

    commit

--- app/calculator/src/main/java/com/example/calculator/MainActivity.java
+++ app/calculator/src/main/java/com/example/calculator/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.calculator);
+        setContentView(R.layout.calcu_linear);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(
